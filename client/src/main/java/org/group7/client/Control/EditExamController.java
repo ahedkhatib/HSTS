48b5670cee457e627dca9cd1fa84a422a4e44939
Alaa Khamis <alaakhamis@outlook.com>
2023-06-04 19:45:53 +0300

    wroking one editing exam

---
+++ client/src/main/java/org/group7/client/Control/EditExamController.java
@@ -0,0 +1,13 @@
+package org.group7.client.Control;
+
+import org.group7.client.Boundary.EditExamBoundary;
+
+public class EditExamController extends Controller{
+
+    EditExamBoundary boundary;
+
+    public EditExamController(EditExamBoundary boundary){
+        this.boundary = boundary;
+    }
+
+}
